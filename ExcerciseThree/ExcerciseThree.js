dfd7c0a9a7c49e1f9aa149ebfc4bab912fd1979e
Hike <mikialebachew@gmail.com>
2019-05-01 00:54:40 +0600

    Exercise Three.

--- ExcerciseThree/ExcerciseThree.js
+++ ExcerciseThree/ExcerciseThree.js
@@ -1,7 +1,7 @@
 function isWeekend(){
 
     const todayDate = new Date();
-    const day = 6;//todayDate.getDay();	// 0 - 6 (0 is sunday)
+    const day = todayDate.getDay();	// 0 - 6 (0 is sunday)
     const days = ['weekend','weekday'];
 
     console.log( days[ Math.floor(Math.sqrt(7 % (day+1))) ] );
