4b9bda26ac451a80ddbdfad6f7a6a9735ff75c90
Hike <mikialebachew@gmail.com>
2019-04-30 15:20:51 +0600

    Exercise One.

--- ExcersiceOne/ExcerciseOne.js
+++ ExcersiceOne/ExcerciseOne.js
@@ -55,4 +55,32 @@
 
 
 
+
+    /**
+     *  Filter words using async and await
+     */
+    String.prototype.filterWords = async function(bannedWords){
+
+        const promise = new Promise((resolve, reject) => {
+
+            let result = this;
+            for(let word of bannedWords){
+                result = result.replace(new RegExp(word, 'g'), '***');
+            }
+
+            if(result != this)
+                resolve(result);
+            else
+                reject(result);
+
+        });
+
+        const result = await promise; // wait till the promise resolve
+        console.log(result);
+
+    }
+
+    "what a stupid idea".filterWords(['shit','danm','stupid']);
+    // outPut:  what a *** idea
+
 })();
