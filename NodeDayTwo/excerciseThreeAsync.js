6cf8bc3c56143fb1346df51207121109cc4d29a2
Hike <mikialebachew@gmail.com>
2019-05-04 20:26:06 +0600

    File response synchronously, asynchronously and using stream

--- NodeDayTwo/excerciseThreeAsync.js
+++ NodeDayTwo/excerciseThreeAsync.js
@@ -1,2 +1,33 @@
 const http = require('http');
+const fs = require('fs');
 const { Subject } = require('rxjs');
+
+const subject$ = new Subject();
+
+async function readFile(){
+    const file = await fs.readFile('./book.pdf');
+    return file;
+}
+
+function sendFile(obj){
+    obj.res.writeHead(200, {'Content-Type': 'application/pdf'});
+    obj.res.end(readFile());
+}
+
+subject$.subscribe(sendFile);
+
+server.on('request', (request, response) => {
+
+    if(request.url === '/favicon.ico'){
+        response.writeHead(200, {'Content-Type':'image/x-icon'});
+        response.end();
+        return;
+    }
+
+    subject$.next({'req':request, 'res':response});
+
+});
+
+server.listen(1616);
+
+subject$.subscribe();
